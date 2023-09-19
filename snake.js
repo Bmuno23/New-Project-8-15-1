f195fe5d3f5451afccb0f0a4719e89cdf2d54d68
Bruce Munoz <brucemuno@gmail.com>
2023-09-18 21:46:43 -0400

    Create Snake head and food Add arrow key controls to snake head and removed ability to move the exact opposite direction ex arrow up then down Snake head will now appear above food Add Color to snake and food Add border to canvas Add speed function for every time the key is pressed it will move one block. Add randomization of food and head upon start of game

--- snake.js
+++ snake.js
@@ -1,4 +1,4 @@
-
+//var tells javascript you're declaring a variable
 //board
 var blockSize = 25;
 var rows = 20;
@@ -6,18 +6,70 @@ var cols = 20;
 var board;
 var context;
 
+//snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+//speed for snake
+var velocityX = 0;
+var velocityY = 0;
+
+//food
+var foodX;
+var foodY;
 
 window.onload = function() {
     board = document.getElementById("board");
     board.height = rows * blockSize;
     board.width = cols * blockSize;
     context = board.getContext("2d"); //Used for drawing on the board
-
-    update();
-
+    
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
+    //update();
+    setInterval(update, 1000/10); //100 milliseconds 
 }
 
 function update(){
-    context.fillStyle="black";
+    context.fillStyle="white";
     context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle="blue";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    if (snakeX == foodX && snakeY == foodY){
+        placeFood();
+    }
+
+    context.fillStyle="grey";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+}
+
+//Change direction using arrow keys
+function changeDirection(e) {
+    if (e.code == "ArrowUp" && velocityY != 1){  // != 1 makes sure you can't go the same exact opposite direction ex up then down
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.code == "ArrowDown" && velocityY != -1){
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.code == "ArrowLeft" && velocityX != 1){
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if (e.code == "ArrowUp" && velocityX != -1){
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+function placeFood(){
+    //0-1) *cols --> 90-19.00) --> (--19) * 25 
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * cols) * blockSize;
 }
